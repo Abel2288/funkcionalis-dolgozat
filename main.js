558a70968f233ab7e30c175689789ae6dece90eb
Szabó Ábel Ákos <szabo.abel@petrik.hu>
2023-11-06 16:44:50 +0100

    kesz

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 import './style.css'
+import data from './quotes.json' 
+console.log(data); 
 
+
+const lista = Array.from(data.quotes)
+
+
+const sortedAuthor =lista.sort((a,b) => {
+  if ( a.author < b.author ){
+    return -1;
+  }
+  if ( a.author > b.author ){
+    return 1;
+  }
+  return 0;
+});
+
+
+      
+console.log(sortedAuthor);
